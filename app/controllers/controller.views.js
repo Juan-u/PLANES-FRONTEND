55c331b5ccee3ddf61639e22c6efc7ddd9b16a38
Juan-u <u20192184291@usco.edu.co>
2026-09-18 15:19:52 -0500

    Railway

--- app/controllers/controller.views.js
+++ app/controllers/controller.views.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = `${process.env.BACKEND_URL}/api`;
 
 // ============================================================
 // AUTH
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import session from 'express-session';
 import rutas from './app/routes/routes.views.js';
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -13,15 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(session({
-    secret           : 'secreto_planes_accion',
-    resave           : false,
+    secret: process.env.SESSION_SECRET,
+    resave: false,
     saveUninitialized: false,
-    cookie           : { maxAge: 2 * 60 * 60 * 1000 }
+    cookie: {
+        maxAge: 2 * 60 * 60 * 1000,
+        secure: process.env.NODE_ENV === "production"
+    }
 }));
 
 app.use('/', rutas);
 
-app.listen(PORT, () => {
-    console.log(`Frontend corriendo en http://localhost:${PORT}`);
-    console.log(`Asegúrate de que el backend esté corriendo en http://localhost:3000`);
+app.listen(PORT, "0.0.0.0",() => {
+    console.log(`Frontend corriendo en ${PORT}`);
 });
