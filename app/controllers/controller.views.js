c870513380936e50cf514b8ea2df8ae1af5c8507
Juan-u <u20192184291@usco.edu.co>
2026-09-20 22:35:14 -0500

    Hola

--- app/controllers/controller.views.js
+++ app/controllers/controller.views.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const API_URL = `${process.env.BACKEND_URL}/api`;
+const API_URL = `${process.env.BACKEND_URL}/api`;   
 
 // ============================================================
 // AUTH
--- index.js
+++ index.js
@@ -5,12 +5,15 @@ import rutas from './app/routes/routes.views.js';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+app.set('trust proxy', 1);
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
